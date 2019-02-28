9231e9f45a1b83a8ba3806588517e53595be5e59
Michael Wnek <michaelwnek1@gmail.com>
2019-02-28 20:18:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 def oxford_comma(array)
-  return "kiwi"
+  if array.length == 2 
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and")
+  end 
+  array.join
 end
-
-
-def oxford_comma(array)
-  return "kiwi and durian"
-end
+    
